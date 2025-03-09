c866390a20a837d080bb9915c6a3cdd0a919112d
artistic8 <aadel.iimam@gmail.com>
2025-03-09 10:22:17 +0800

    ...

--- 0308S3/bets1027.php
+++ 0308S3/bets1027.php
@@ -1,59 +1,6 @@
 <?php
 
 return [
-	'1' => [
-		/**
-		Race 1
-		*/
-		'favorites' => '1, 7, 9',
-		'runners' => '1, 2, 3, 5, 6, 7, 8, 9',
-		'win inter' => '3, 8',
-		'qin inter' => '3, 8, 9',
-		'trio inter' => '3, 5, 8, 9',
-		'bets' => [
-		],
-		'total bets' => 0,
-	],
-	'2' => [
-		/**
-		Race 2
-		*/
-		'favorites' => '2, 7, 10, 12',
-		'runners' => '1, 2, 3, 4, 6, 7, 8, 9, 10, 11, 12',
-		'win inter' => '7',
-		'qin inter' => '2, 6, 7, 9, 12',
-		'trio inter' => '1, 2, 6, 7, 8, 9, 12',
-		'bets' => [
-			'qin(union 1027)' => '2, 7, 10, 12',
-		],
-		'total bets' => 60,
-	],
-	'3' => [
-		/**
-		Race 3
-		*/
-		'favorites' => '4, 7, 9, 17',
-		'runners' => '2, 3, 4, 5, 6, 7, 8, 9, 12, 13, 17, 19',
-		'win inter' => '',
-		'qin inter' => '',
-		'trio inter' => '',
-		'bets' => [
-		],
-		'total bets' => 0,
-	],
-	'4' => [
-		/**
-		Race 4
-		*/
-		'favorites' => '1, 2, 7, 8, 9, 10, 13',
-		'runners' => '1, 2, 4, 5, 7, 8, 9, 10, 11, 12, 13, 14',
-		'win inter' => '',
-		'qin inter' => '',
-		'trio inter' => '',
-		'bets' => [
-		],
-		'total bets' => 0,
-	],
 ];
-//total qin: -60
+//total qin: 0
 //total: 0
